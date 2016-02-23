d75b01629bc820b85dc787b6c2d1972fef39cb14
randallkanna <randallkanna@gmail.com>
2016-02-22 23:09:21 -0800

    initial commit

---
+++ iRecord/iRecord/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iRecord
+//
+//  Created by Randall Kanna on 2/22/16.
+//  Copyright © 2016 Randall Kanna. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
